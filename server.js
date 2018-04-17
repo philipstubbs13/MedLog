2e170667531e44c3ea3e244fd05eff8bda39536c
Kayla <k.kotewall@gmail.com>
2018-04-17 08:47:40 -0500

    user created with associated routes

--- server.js
+++ server.js
@@ -8,11 +8,74 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// When the server starts, create and save a new User document to the db
+// The "unique" rule in the User model's schema will prevent duplicate users from being added to the server
+db.User.create({ name: "John Doe" })
+  .then(function(dbUser)
+    console.log(dbUser);
+  })
+  .catch(function(err) {
+    console.log(err.message);
+  });
+
+
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+
+// Route for retrieving all Users from the db
+app.get("/user", function(req, res) {
+  // Find all Users
+  db.User.find({})
+    .then(function(dbUser) {
+      // If all Users are successfully found, send them back to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send the error back to the client
+      res.json(err);
+    });
+});
+
+// Route for saving a new Health Log to the db and associating it with a User
+app.post("/submit", function(req, res) {
+  // Create a new Note in the db
+  db.healthLog.create(req.body)
+    .then(function(dbHealthLog) {
+      // If a Health Log was created successfully, find one User and push the new Log's _id to the User's `healthLog` array
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.User.findOneAndUpdate({}, { $push: { notes: dbHealthLog._id } }, { new: true });
+    })
+    .then(function(dbUser) {
+      // If the User was updated successfully, send it back to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+});
+
+// Route to get all User's and populate them with their notes
+app.get("/populateduser", function(req, res) {
+  // Find all users
+  db.User.find({})
+    // Specify that we want to populate the retrieved users with any associated notes
+    .populate("healthLog")
+    .then(function(dbUser) {
+      // If able to successfully find and associate all Users and Health Logs, send them back to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+});
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
