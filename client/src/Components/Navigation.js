f81a4f1b4de73fed79d9b73f2a6be8b6f5818fb7
Phil S <philipstubbs13@gmail.com>
2018-04-23 23:52:17 -0500

    minor tweak

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -23,6 +23,7 @@ const NavigationNonAuth = () =>
   <ul>
     <li><Link to={routes.LANDING}>Landing</Link></li>
     <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+    <li><SignOutButton /></li>
   </ul>
 
 export default Navigation;
