057915389881d0468fbb35b07eef0e19e917cf34
Phil S <philipstubbs13@gmail.com>
2018-04-30 22:13:12 -0500

    404 page added

---
+++ client/src/containers/NoMatch/NoMatch.js
@@ -0,0 +1,76 @@
+// 404 Not found page
+// https://tylermcginnis.com/react-router-handling-404-pages/
+
+// Importing React since we are using React.
+import React, { Component } from 'react';
+// Importing UI components and style from material-ui-next
+import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+// Importing UI components from rebass.
+import { Container } from 'rebass';
+// import third-party routing library (react-router-dom)
+import { Link } from 'react-router-dom';
+
+// Styling
+const styles = {
+  // Tell Material-UI what's the font-size on the html element is.
+  root: {
+    flexGrow: 1,
+  },
+  headline: {
+    marginTop: 30,
+  },
+  title: {
+    marginTop: 30,
+  },
+  text: {
+    marginTop: 20,
+  },
+  button: {
+    marginTop: 20,
+    padding: 15,
+    backgroundColor: '#007AC1',
+    color: 'white',
+  },
+
+};
+
+// 404 Not Found Page
+class NoMatch extends Component {
+  render() {
+    const { classes } = this.props;
+    return [
+      <Container>
+        <Grid item xs={12} className={classes.headline}>
+          <Grid container spacing={16} className={classes.root} justify="center">
+            <Typography variant="display2" className={classes.title}>
+              Oops! Something broke...
+            </Typography>
+          </Grid>
+        </Grid>,
+
+        <div className="main-content-section">
+          <Grid item xs={12} className={classes.headline}>
+            <Grid container spacing={16} className={classes.root} justify="center">
+              <Typography variant="title" className={classes.title}>
+                  Sorry, this page could not be found.
+              </Typography>
+              <Typography variant="subheading" className={classes.text}>
+                There are a lot of potential reasons why this page is <a href="https://en.wikipedia.org/wiki/HTTP_404" target="_blank">404 (not found)</a>. It could be because of a mistyped URL, a faulty referral from another site, an out-of-date search engine listing, or we simply deleted a file. Don't waste your time trying to figure it out. Instead, let's get you back to the home page. To get back to the home page, click the button below.
+              </Typography>
+              <Button variant="raised" color="primary" className={classes.button} component={Link} to="/home">
+                Take me home
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>,
+    ];
+  }
+}
+
+// Exporting the NoMatch component
+// so that the App.js file can render the Login page.
+export default withStyles(styles)(NoMatch);
