934900ec745c6f5c01d9f52f0777ed73b9df2035
Sara Bracewell <bracewell.sara@gmail.com>
2018-05-04 11:57:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -47,88 +47,69 @@ app.use(routes);
 //   .catch(function(err) {
 //     console.log(err.message);
 //   });
-// app.get('/', (req, res, next) => {
-//   res.json('req.user is: ' + req.user)
-// })
-// app.post('/login',
-//   passport.authenticate('local', { failureRedirect: '/' }), (req, res) => res.json('logged in!'))
-// app.post('/logout', (req, res) => { req.logout(); res.redirect('/'); })
 
-// app.post('/signup', (req, res, next) => {
-//   var user = new User({ username: req.body.username, password: req.body.password })
-//   user.save().then(data => console.log(data)).catch(err => console.log(err))
-//   res.redirect('/')
-// })
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
-// app.get('/profile',
-//   passport.authenticate('bearer', { session: false }),
-//   function (req, res) {
-//     res.json(req.user);
-//   });
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 
-// // Route for retrieving all Users from the db
-// app.get('/user', function (req, res) {
-//   // Find all Users
-//   db.User.find({})
-//     .then(function (dbUser) {
-//       // If all Users are successfully found, send them back to the client
-//       res.json(dbUser);
-//     })
-//     .catch(function (err) {
-//       // If an error occurs, send the error back to the client
-//       res.json(err);
-//     });
-// });
+// Route for retrieving all Users from the db
+app.get('/user', function (req, res) {
+  // Find all Users
+  db.User.find({})
+    .then(function (dbUser) {
+      // If all Users are successfully found, send them back to the client
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      // If an error occurs, send the error back to the client
+      res.json(err);
+    });
+});
 
 
-// // Route for saving a new Health Log to the db and associating it with a User
-// app.post('/submit', function (req, res) {
-//   // Create a new Note in the db
-//   db.healthLog.create(req.body)
-//     .then(function (dbHealthLog) {
-//       // If a Health Log was created successfully,
-//       // find one User and push the new Log's _id to the User's `healthLog` array
-//       // { new: true } tells the query that we want it to return the updated User --
-//       // it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another
-//       // `.then` which receives the result of the query
-//       return db.User.findOneAndUpdate({}, { $push: { notes: dbHealthLog._id } }, { new: true });
-//     })
-//     .then(function (dbUser) {
-//       // If the User was updated successfully, send it back to the client
-//       res.json(dbUser);
-//     })
-//     .catch(function (err) {
-//       // If an error occurs, send it back to the client
-//       res.json(err);
-//     });
-// });
+// Route for saving a new Health Log to the db and associating it with a User
+app.post('/submit', function (req, res) {
+  // Create a new Note in the db
+  db.healthLog.create(req.body)
+    .then(function (dbHealthLog) {
+      // If a Health Log was created successfully,
+      // find one User and push the new Log's _id to the User's `healthLog` array
+      // { new: true } tells the query that we want it to return the updated User --
+      // it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another
+      // `.then` which receives the result of the query
+      return db.User.findOneAndUpdate({}, { $push: { notes: dbHealthLog._id } }, { new: true });
+    })
+    .then(function (dbUser) {
+      // If the User was updated successfully, send it back to the client
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+});
 
-// // Route to get all User's and populate them with their notes
-// app.get('/populateduser', function (req, res) {
-//   // Find all users
-//   db.User.find({})
-//     // Specify that we want to populate the retrieved users with any associated notes
-//     .populate('healthLog')
-//     .then(function (dbUser) {
-//       // If able to successfully find and associate all Users and Health Logs,
-//       // send them back to the client
-//       res.json(dbUser);
-//     })
-//     .catch(function (err) {
-//       // If an error occurs, send it back to the client
-//       res.json(err);
-//     });
-// });
+// Route to get all User's and populate them with their notes
+app.get('/populateduser', function (req, res) {
+  // Find all users
+  db.User.find({})
+    // Specify that we want to populate the retrieved users with any associated notes
+    .populate('healthLog')
+    .then(function (dbUser) {
+      // If able to successfully find and associate all Users and Health Logs,
+      // send them back to the client
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+});
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reacthealthtracker');
